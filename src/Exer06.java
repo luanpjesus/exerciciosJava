1cfb636def251f82f25bac606e0fe097712de3dd
ljp <luanpjesus@gmail.com>
2021-08-20 11:47:30 -0300

    Commit do Exercicio 06 do java

---
+++ src/Exer06.java
@@ -0,0 +1,12 @@
+public class Exer06 {
+
+    public static void main(String[] args) {
+        
+        for(int i=1; i<= 20; i++){
+            System.out.print(i + " ");
+        }
+
+    }
+    
+}
+
